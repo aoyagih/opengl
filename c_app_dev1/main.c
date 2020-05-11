5d11f59ff452d96023fc8e743dbe4df075b41689
aoyagih <hiroki3290@gmail.com>
2020-05-11 22:02:49 +0900

    Initial Commit

---
+++ c_app_dev1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_app_dev1
+//
+//  Created by Aoyagi Hiroki on 2020/05/11.
+//  Copyright © 2020 aoyagih. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
